2696896a299b7fd197ea8426a1539523fdefc1be
Meagan <MeaganTruong1@yahoo.com>
2023-11-28 19:27:28 -0600

    base deliverables

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+const [listings, setListings] = useState([])
+const [search, setSearch] = useState("")
+
+useEffect(() => {
+  fetch("http://localhost:6001/listings")
+    .then(resp => resp.json())
+    .then(data => setListings(data))
+}, [])
+
+const removeListing = (id) => {
+  setListings(listings.filter(listing => listing.id !== id))
+}
+
+const handleSearchListing = (searchedName) => {
+  setSearch(searchedName)
+}
+
+const filterListings = listings.filter(listing => 
+  listing.description.toLowerCase().includes(search.toLowerCase())
+)
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header onSearch={handleSearchListing}/>
+      <ListingsContainer listings={filterListings} removeListing={removeListing}/>
     </div>
   );
 }
--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -1,21 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
-function ListingCard() {
+function ListingCard({ listing, removeListing }) {
+const [favorited, setFavorited] = useState(false)
+
+const toggleFavorite = () => {
+  setFavorited(!favorited)
+}
+
+const renderFavoriteButton = () => {
+  const buttonClass = `emoji-button favorite ${favorited ? "active" : ""}`
+  return(
+    <button className={buttonClass} onClick={toggleFavorite}>
+      {favorited ? "★" : "☆"}
+    </button>
+  )
+}
+
+const handleDelete = () => {
+  fetch(`http://localhost:6001/listings/${listing.id}`, {
+    method: "DELETE",
+  })
+  .then(resp => {
+    if (resp.ok) {
+      removeListing(listing.id)
+    }
+  })
+}
   return (
     <li className="card">
       <div className="image">
         <span className="price">$0</span>
-        <img src={"https://via.placeholder.com/300x300"} alt={"description"} />
+        <img src={listing.image} alt={listing.description} />
       </div>
       <div className="details">
-        {true ? (
-          <button className="emoji-button favorite active">★</button>
-        ) : (
-          <button className="emoji-button favorite">☆</button>
-        )}
-        <strong>{"description"}</strong>
-        <span> · {"location"}</span>
-        <button className="emoji-button delete">🗑</button>
+        {renderFavoriteButton()}
+        <strong>{listing.description}</strong>
+        <span> · {listing.location}</span>
+        <button className="emoji-button delete" onClick={handleDelete}>🗑</button>
       </div>
     </li>
   );
